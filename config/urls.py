21f72742f551f4f9564c9fa7808a6f2b4671ea49
pkanoontutor <pkanoontutor@gmail.com>
2026-01-01 23:16:08 +0700

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from core.views import home  # 👈 เพิ่มบรรทัดนี้
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),
+    path("", home, name="home"),        # 👈 หน้า Home (www.pkanoontutor.com)
+    path("", include("core.urls")),     # 👈 URL อื่น ๆ ของ core (student-portal ฯลฯ)
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

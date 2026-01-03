13ea36a65037534801090cc5de35be7f076bd403
pkanoontutor <pkanoontutor@gmail.com>
2026-01-03 15:16:47 +0700

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from core.views import home  # 👈 เพิ่มบรรทัดนี้
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("adminlublub/", admin.site.urls),
     path("", home, name="home"),        # 👈 หน้า Home (www.pkanoontutor.com)
     path("", include("core.urls")),     # 👈 URL อื่น ๆ ของ core (student-portal ฯลฯ)
 ]

2463c1da0f414c4f26271d3f1345b636ec72189f
pkanoontutor <pkanoontutor@gmail.com>
2026-01-04 16:47:15 +0700

    Create 0008_add_time_slot_real.py

---
+++ core/migrations/0008_add_time_slot_real.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0007_school_remove_enrollment_closed_at_and_more'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='tutoringclass',
+            name='time_slot',
+            field=models.CharField(
+                max_length=20,
+                choices=[
+                    ('sat_morning', 'เสาร์เช้า'),
+                    ('sat_afternoon', 'เสาร์บ่าย'),
+                    ('sun_morning', 'อาทิตย์เช้า'),
+                    ('sun_after_noon', 'อาทิตย์บ่าย'),
+                ],
+                default='sat_morning',
+            ),
+        ),
+    ]

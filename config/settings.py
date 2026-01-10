23214cce6ddfd2f873d6c40bb2df16d4eff59013
pkanoontutor <pkanoontutor@gmail.com>
2026-01-10 14:02:49 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -166,7 +166,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # -------------------------------------------------------------------
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_URL = "/admin/login/"
+LOGIN_URL = "/adminlublub/login/"
 LOGIN_REDIRECT_URL = "/dashboard/"
 
 MEDIA_URL = "/media/"
